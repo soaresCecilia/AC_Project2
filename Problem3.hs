773f1295cd84010db1474c00b8a6cafce408045c
Cecilia Soares <ceciliasoares@MBP-de-Cecilia.home>
2021-06-15 02:11:46 +0100

    Problem3.hs

--- Problem3.hs
+++ Problem3.hs
@@ -1,59 +1,78 @@
 module Problem3 where
+
 import System.IO
 
 data Node = A | B | C | D | E | F deriving (Show,Eq,Ord)
 
-type Path = [Node]
-
-
-loadPaths = do
-  ls <- fmap lines (readFile "file.txt")
-  return ls
-
-transPath :: (Node, Node) -> String
-transPath (a, b) = show a ++ "," ++ show b
-
-isAdjacent :: (Node, Node) -> [String] -> Bool
-isAdjacent (a, b) paths = elem (transPath (a, b)) paths
-
-adj :: (Node, Node) -> IO Bool 
-adj(a, b) = do
-           paths <- loadPaths
-           return (isAdjacent (A, D) paths)
-
-
-
--- 2. Auxiliary functions
-adjacentNodes :: Node -> [Node] -> [Node]
-adjacentNodes n ns = filter (\x -> adj(n,x)) ns
-
 allNodes :: [Node]
 allNodes = [A,B,C,D,E,F]
 
-choice :: ([a],[a]) -> [a]
-choice = uncurry (++)
+type Path = [Node]
+type Graph = [(Node,Node)]
 
--- 3. Main body
-{- For each node a in ns, if a is not already in p the function
+-- converts a char into a node
+convertToNode :: Char -> Node
+convertToNode c = case c  of 
+                   'A' -> A
+                   'B' -> B
+                   'C' -> C
+                   'D' -> D
+                   'E' -> E
+                   'F' -> F
+
+-- string from file become a tuple of nodes
+strToTup :: [Char] -> (Node, Node)
+strToTup (h:comma:b:t) = (convertToNode h, convertToNode b) 
+
+
+-- transforming a list of string into a Graph
+transformIntoGraph :: [String] -> Graph 
+transformIntoGraph lines = map (\x -> strToTup x) lines
+
+
+-- reading a file into a IO Graph
+loadPaths :: IO Graph
+loadPaths = do
+  ls <- fmap lines (readFile "file.txt")
+  let lines = transformIntoGraph ls
+  return lines
+
+
+{- 
+Returns a list of nodes that are adjacent of certain nodes
+taking the Graph into account.
+-}
+adjacentNodes :: Node -> [Node] -> Graph -> [Node]
+adjacentNodes n ns g = filter (\x -> elem (n,x) g) ns
+
+
+{-
+For each node a in ns, if a is not already in p the function
    creates a new path by adding to the end of p the element a.
 -}
--- !! To implement !!
-addtoEnd :: Path -> [Node] -> [Path]
-addtoEnd p ns | null as = [p]
-              | otherwise = map (\x -> p ++ [x]) as
-              where as = filter ( `notElem` p) (adjacentNodes (last p) ns)
+addtoEnd :: Path -> [Node] -> Graph -> [Path]
+addtoEnd p ns g | null as = [p]
+                | otherwise = map (\x -> p ++ [x]) as
+                where as = filter (`notElem` p) (adjacentNodes (last p) ns g)
+
+-- Computes all Hamiltonian cycles given a node and our Graph
+hCycles :: Node -> Graph -> [Path]
+hCycles n g = do
+     { x <- hCyclesAux (length allNodes) [[n]] g;
+       t <- filter (\l -> (elem(last l, n) g)  && length l == (length allNodes)) [x];
+       map (\l -> l ++ [n]) [t] }
+
+{- 
+For all list of paths given we will compute all the possible paths 
+without repeating nodes.
+-}
+hCyclesAux :: Int -> [Path] -> Graph -> [Path]
+hCyclesAux 0 path graph = path
+hCyclesAux i path graph = hCyclesAux (i-1) (concatMap (\x -> (addtoEnd x allNodes graph)) path) graph
 
 
 
 -- Computes all Hamiltonian cycles starting from a given node
--- !! To implement !!
-hCycles :: Node -> [Path]
-hCycles n = do
-    { p <- addtoEnd [n] allNodes;
-      x <- hCyclesAux (length allNodes) [p];
-      t <- filter (\l -> adj(last l, n) && length l == (length allNodes)) [x];
-      map (\l -> l ++ [n]) [t] }
-
-hCyclesAux :: Int -> [[Node]] -> [[Node]]
-hCyclesAux 0 x = x
-hCyclesAux i x = hCyclesAux (i-1) (concatMap (`addtoEnd` allNodes) x)
+myCycles :: Node -> IO [Path]
+myCycles n = do g <- loadPaths
+                return (hCycles n g)
